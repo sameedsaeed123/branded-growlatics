0ed321f6d7281d645acdd6f1bbf79749ab040c6b
sameed123 <l1f22bscs0956@ucp.edu.pk>
2025-12-30 08:39:58 +0500

    updated forms2

--- assets/js/ajax-form.js
+++ assets/js/ajax-form.js
@@ -62,6 +62,35 @@ $(function() {
 			return false;
 		}
 
+		// If the form posts to Web3Forms, submit via JSON to their API and show inline success/error
+		if (action.indexOf('api.web3forms.com') !== -1) {
+			var $submitBtn = $(form).find('button[type="submit"]');
+			var originalBtnHtml = $submitBtn.html();
+			$submitBtn.prop('disabled', true).text('Sending...');
+			var fd = new FormData(form[0]);
+			var obj = {};
+			fd.forEach(function(value, key) { obj[key] = value; });
+			fetch(action, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(obj)
+			})
+			.then(function(res) { return res.json(); })
+			.then(function(data) {
+				var msg = (data && (data.message || data.success && data.message)) ? data.message : 'Thanks! Your message was sent.';
+				showEmailJsMessage($(form), msg);
+				$(form)[0].reset();
+			})
+			.catch(function(err) {
+				console.error('Web3Forms error', err);
+				showEmailJsError('Failed to send message.');
+			})
+			.finally(function() {
+				$submitBtn.prop('disabled', false).html(originalBtnHtml);
+			});
+			return false;
+		}
+
 		// Stop the browser from submitting the form for normal AJAX handler.
 		e.preventDefault();
 
@@ -136,6 +165,107 @@ $(function() {
 				.finally(function() {
 					$btn.prop('disabled', false).html(originalBtn);
 				});
+			});
+		});
+	});
+
+	// Handle any footer/site forms that post to FormSubmit so they submit via AJAX and show inline messages
+	$('form[action*="formsubmit.co"]').not('#contact-form').each(function() {
+		var $ff = $(this);
+		$ff.on('submit', function(e) {
+			e.preventDefault();
+			var action = $ff.attr('action') || '';
+			var $btn = $ff.find('button[type="submit"], .tp-btn-subscribe');
+			var originalBtn = $btn.html();
+			$btn.prop('disabled', true).text('Sending...');
+			var fd = new FormData($ff[0]);
+			var ajaxUrl = action.replace(/\/+$/, '');
+			if (ajaxUrl.indexOf('/ajax/') === -1) {
+				ajaxUrl = ajaxUrl.replace('https://formsubmit.co/', 'https://formsubmit.co/ajax/');
+			}
+			$.ajax({
+				type: 'POST',
+				url: ajaxUrl,
+				data: fd,
+				contentType: false,
+				processData: false,
+				dataType: 'json',
+				headers: { 'Accept': 'application/json' }
+			})
+			.done(function(data) {
+				var msg = (data && data.message) ? data.message : 'Thanks! Your message was sent.';
+				showEmailJsMessage($ff, msg);
+				$ff[0].reset();
+			})
+			.fail(function(jqXHR) {
+				var errMsg = 'Oops! An error occured and your message could not be sent.';
+				if (jqXHR && jqXHR.responseJSON && jqXHR.responseJSON.message) {
+					errMsg = jqXHR.responseJSON.message;
+				} else if (jqXHR && jqXHR.responseText) {
+					errMsg = jqXHR.responseText;
+				}
+				var $m = $ff.next('.ajax-response');
+				if ($m.length === 0) {
+					$m = $('<p class="ajax-response"></p>');
+					$ff.after($m);
+				}
+				$m.removeClass('success').addClass('error').text(errMsg);
+			})
+			.always(function() {
+				$btn.prop('disabled', false).html(originalBtn);
+			});
+		});
+	});
+
+	// Handle footer/site newsletter forms that post to Web3Forms API
+	$('form[action*="api.web3forms.com"]').not('#contact-form').each(function() {
+		var $ff = $(this);
+		$ff.on('submit', function(e) {
+			e.preventDefault();
+			var action = $ff.attr('action') || '';
+			var $btn = $ff.find('button[type="submit"], .tp-btn-subscribe');
+			var originalBtn = $btn.html();
+			$btn.prop('disabled', true).text('Sending...');
+			var fd = new FormData($ff[0]);
+			var obj = {};
+			// validate terms checkbox when present
+			var $agree = $ff.find('input[name="agree_terms"]');
+			if ($agree.length && !$agree.prop('checked')) {
+				var $m = $ff.next('.ajax-response');
+				if ($m.length === 0) { $m = $('<p class="ajax-response"></p>'); $ff.after($m); }
+				$m.removeClass('success').addClass('error').text('Please agree to the terms and policies.');
+				$btn.prop('disabled', false).html(originalBtn);
+				return;
+			}
+			fd.forEach(function(value, key) { obj[key] = value; });
+			fetch(action, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(obj)
+			})
+			.then(function(res) { return res.json(); })
+			.then(function(data) {
+				var defaultMsg = 'Thanks! Your request was sent.';
+				var msg = defaultMsg;
+				if (obj.subject && obj.subject === 'Newsletter Signup') {
+					msg = 'Form submitted successfully!';
+				} else if (data && (data.message || (data.success && data.message))) {
+					msg = data.message || msg;
+				}
+				showEmailJsMessage($ff, msg);
+				$ff[0].reset();
+			})
+			.catch(function(err) {
+				console.error('Web3Forms error', err);
+				var $m = $ff.next('.ajax-response');
+				if ($m.length === 0) {
+					$m = $('<p class="ajax-response"></p>');
+					$ff.after($m);
+				}
+				$m.removeClass('success').addClass('error').text('Failed to send message.');
+			})
+			.finally(function() {
+				$btn.prop('disabled', false).html(originalBtn);
 			});
 		});
 	});
